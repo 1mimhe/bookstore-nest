9777eff4e348f41a68135b4b856bbbc562f2c9b3
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-03 16:41:06 +0330

    Add Typeorm and Redis config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,81 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_PIPE } from '@nestjs/core';
+import * as session from 'express-session';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
+import RedisStore from 'connect-redis';
+import { createClient } from 'redis';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: process.env.NODE_ENV === 'test' ? 'sqlite' : 'mysql',
+          host: config.get('DB_HOST', 'localhost'),
+          port: config.get<number>('DB_PORT', 3306),
+          username: config.getOrThrow<string>('DB_USERNAME'),
+          password: config.getOrThrow<string>('DB_PASSWORD'),
+          database: config.getOrThrow<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: process.env.NODE_ENV !== 'production',
+        };
+      },
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          stores: [createKeyv(config.getOrThrow<string>('REDIS_URL'))]
+        };
+      },
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    private readonly config: ConfigService
+  ) {}
+
+  async configure(consumer: MiddlewareConsumer) {
+    const redisClient = await createClient({
+      url: this.config.getOrThrow<string>('REDIS_URL')
+    }).connect();
+
+    consumer
+      .apply(
+        session({
+          secret: this.config.getOrThrow<string>('SESSION_SECRET'),
+          resave: false,
+          saveUninitialized: false,
+          store: new RedisStore({
+            client: redisClient,
+            prefix: 'sess'
+          }),
+          cookie: {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: this.config.get<number>('COOKIE_MAX_AGE', 15 * 24 * 3600 * 1000) // 15 days
+          }
+        }),
+      )
+      .forRoutes('*');
+  }
+}
