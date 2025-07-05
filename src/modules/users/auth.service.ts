5af30b1a929de0deccc34e410519ab9867d1cde9
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-05 20:13:34 +0330

    Add signup service and controller

--- src/modules/users/auth.service.ts
+++ src/modules/users/auth.service.ts
@@ -1,14 +1,67 @@
-import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
-import { User } from "./entities/user.entity";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Role } from "./entities/role.entity";
-import { CreateUserDto } from "./dtos/create-user.dto";
+import { ConflictException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Roles } from './entities/role.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { pbkdf2, randomBytes } from 'crypto';
+import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private usersService: UsersService,
     @InjectRepository(User) private userRepo: Repository<User>,
-    @InjectRepository(Role) private roleRepo: Repository<Role>
   ) {}
+
+  async signup(userDto: CreateUserDto, roles: Roles[] = [Roles.Customer]) {
+    const { password, email, phoneNumber, ...userData } = userDto;
+
+    const conflicts = await this.usersService.checkUniqueConstraints(userData.username, phoneNumber, email);
+    if (conflicts.length > 0) {
+      throw new ConflictException(conflicts);
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+    const user = this.userRepo.create({
+      ...userData,
+      hashedPassword,
+      contact: {
+        phoneNumber,
+        email,
+      },
+      roles: roles.map((role) => ({ role })),
+    });
+
+    return this.userRepo.save(user);
+  }
+
+  private hashPassword(password: string): Promise<string | never> {
+    const salt = randomBytes(16).toString('hex');
+    return new Promise((resolve, reject) => {
+      pbkdf2(password, salt, 1000, 64, 'sha512', (err, derivedKey) => {
+        if (err) reject(err);
+        resolve(`${salt}:${derivedKey.toString('hex')}`);
+      });
+    });
+  }
+
+  private verifyPassword(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean | never> {
+    return new Promise((resolve, reject) => {
+      const [salt, hash] = hashedPassword.split(':');
+
+      if (!salt || !hash) {
+        reject(new Error('Stored hash is in invalid format.'));
+      }
+
+      pbkdf2(password, salt, 1000, 64, 'sha512', (err, derivedKey) => {
+        if (err) reject(err);
+        const isCorrectPassword = derivedKey.toString('hex') === hash;
+        resolve(isCorrectPassword);
+      });
+    });
+  }
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { ApiBadRequestResponse, ApiConflictResponse, ApiOperation } from '@nestjs/swagger';
+import { ConflictResponseDto, ValidationErrorResponseDto } from 'src/common/dtos/error.dtos';
 
 @Controller('auth')
 export class UsersController {
@@ -9,4 +11,19 @@ export class UsersController {
     private authService: AuthService,
     private usersService: UsersService
   ) {}
+
+  @ApiOperation({ 
+    summary: 'Create a new user account',
+    description: 'Register a new user with unique username, email, and phone number'
+  })
+  @ApiConflictResponse({
+    type: ConflictResponseDto
+  })
+  @ApiBadRequestResponse({
+    type: ValidationErrorResponseDto
+  })
+  @Post('signup')
+  signup(@Body() body: CreateUserDto) {
+    return this.authService.signup(body);
+  }
 }
