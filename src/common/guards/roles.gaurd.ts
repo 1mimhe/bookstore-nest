747600d5cb44d3703ab919889a5ebeeb6110b7fd
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-06 20:42:56 +0330

    Add RolesGuard

---
+++ src/common/guards/roles.gaurd.ts
@@ -0,0 +1,42 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { AuthMessages } from '../enums/auth.messages';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const user = request.user;
+
+    if (!request.user) {
+      throw new ForbiddenException(AuthMessages.AccessDenied);
+    }
+
+    const hasPermission = user?.roles.some(({ role }) =>
+      requiredRoles.includes(role),
+    );
+
+    if (!hasPermission) {
+      throw new ForbiddenException(AuthMessages.AccessDenied);
+    }
+
+    return true;
+  }
+}
