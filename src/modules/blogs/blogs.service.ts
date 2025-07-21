ba85d195a2085bc3f5e016a8d2529eb42493d257
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-21 17:46:12 +0330

    Add dbErrorHandler

--- src/modules/blogs/blogs.service.ts
+++ src/modules/blogs/blogs.service.ts
@@ -1,14 +1,10 @@
-import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Blog } from './blog.entity';
 import { DataSource, In, Repository } from 'typeorm';
-import { Title } from '../books/entities/title.entity';
-import { Publisher } from '../publishers/publisher.entity';
-import { Author } from '../authors/author.entity';
 import { CreateBlogDto } from './dtos/create-blog.dto';
-import { ConflictMessages, NotFoundMessages } from 'src/common/enums/error.messages';
 import { Tag } from '../tags/tag.entity';
-import { DBErrors } from 'src/common/enums/db.errors';
+import { dbErrorHandler } from 'src/common/utilities/error-handler';
 
 @Injectable()
 export class BlogsService {
@@ -41,20 +37,7 @@ export class BlogsService {
       });
 
       return manager.save(Blog, blog).catch((error) => {
-        if (error.code === DBErrors.Conflict) {
-          throw new ConflictException(ConflictMessages.Slug);
-        }
-        if (error.code === DBErrors.ReferenceNotFound) {
-          if (error.message.includes('titleId')) {
-            throw new NotFoundException(NotFoundMessages.Title);
-          }
-          if (error.message.includes('authorId')) {
-            throw new NotFoundException(NotFoundMessages.Author);
-          }
-          if (error.message.includes('publisherId')) {
-            throw new NotFoundException(NotFoundMessages.Publisher);
-          }
-        }
+        dbErrorHandler(error);
         throw error;
       });
     });
