df87b7dc15bfddfba6863db6eea42591ecdfb90c
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-22 22:34:33 +0330

    Add address entity

---
+++ src/modules/users/entities/address.entity.ts
@@ -0,0 +1,32 @@
+import { BaseEntity } from 'src/common/entities/base.entity';
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity('addresses')
+@Index(['user'])
+export class Address extends BaseEntity {
+  @Column()
+  recipientName: string;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column({ default: 'Iran '})
+  country: string;
+
+  @Column()
+  province: string;
+
+  @Column()
+  city: string;
+
+  @Column('text')
+  postalAddress: string;
+
+  @Column()
+  userId: string;
+
+  @ManyToOne(() => User, (user) => user.addresses)
+  @JoinColumn()
+  user: User;
+}
