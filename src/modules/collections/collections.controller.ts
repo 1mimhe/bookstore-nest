27e8c6be6af11a29d7eaed913fe2ebb869093c37
Mohammad Hosseini <mimhe1381@gmail.com>
2025-07-30 12:07:49 +0330

    Add POST /collections

--- src/modules/collections/collections.controller.ts
+++ src/modules/collections/collections.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CollectionsService } from './collections.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { CreateCollectionDto } from './dtos/create-collection.dto';
 
 @Controller('collections')
-export class CollectionsController {}
+export class CollectionsController {
+  constructor(private collectionsService: CollectionsService) {}
+
+  @ApiOperation({
+    summary: 'Create an empty collection'
+  })
+  @Post()
+  async createCollection(@Body() body: CreateCollectionDto) {
+    return this.collectionsService.create(body);
+  }
+}
--- src/modules/collections/collections.service.ts
+++ src/modules/collections/collections.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CollectionBook } from './entities/collection-book.entity';
+import { Repository } from 'typeorm';
+import { Collection } from './entities/collection.entity';
+import { CreateCollectionDto } from './dtos/create-collection.dto';
 
 @Injectable()
-export class CollectionsService {}
+export class CollectionsService {
+  constructor(
+    @InjectRepository(Collection) private collectionRepo: Repository<Collection>,
+    @InjectRepository(CollectionBook) private collectionBookRepo: Repository<CollectionBook>
+  ) {}
+
+  async create(collectionRepo: CreateCollectionDto) {
+    const collection =  this.collectionRepo.create(collectionRepo);
+    return this.collectionRepo.save(collection);
+  }
+}
