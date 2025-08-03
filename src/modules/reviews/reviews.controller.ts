e6ce3790612b253f2c6f0e79e2f73c921c6eb5ad
Mohammad Hosseini <mimhe1381@gmail.com>
2025-08-03 16:47:11 +0330

    Add GET /reviews/blogs/:id controller

--- src/modules/reviews/reviews.controller.ts
+++ src/modules/reviews/reviews.controller.ts
@@ -1,12 +1,28 @@
-import { Body, ClassSerializerInterceptor, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query, Req, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateBookReviewDto } from './dtos/create-review.dto';
 import { ReviewableType } from './entities/review.entity';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { Request } from 'express';
 import { ApiQueryPagination } from 'src/common/decorators/query.decorators';
-import { BookReviewResponseDto, GetBookReviewsResponseDto } from './entities/review-response.entity';
+import {
+  GetBlogReviewsResponseDto,
+  GetBookReviewsResponseDto,
+} from './entities/review-response.entity';
 import { Serialize } from 'src/common/interceptors/serialize.interceptor';
 
 @Controller('reviews')
@@ -15,37 +31,50 @@ export class ReviewsController {
   constructor(private reviewsService: ReviewsService) {}
 
   @ApiOperation({
-    summary: 'Create a review for a book'
+    summary: 'Create a review for a book',
   })
   @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Post('books')
   async createBookReview(
     @Body() body: CreateBookReviewDto,
-    @Req() req: Request
-  ) {    
+    @Req() req: Request,
+  ) {
     const { id } = req.user ?? {};
     const { bookId, ...reviewDto } = body;
-    return this.reviewsService.create(id!, ReviewableType.Book, bookId, reviewDto);
+    return this.reviewsService.create(
+      id!,
+      ReviewableType.Book,
+      bookId,
+      reviewDto,
+    );
   }
 
   @ApiOperation({
-    summary: 'Create a review for a blog'
+    summary: 'Create a review for a blog',
   })
   @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Post('blogs')
   async createBlogReview(
     @Body() body: CreateBookReviewDto,
-    @Req() req: Request
-  ) {    
+    @Req() req: Request,
+  ) {
     const { id } = req.user ?? {};
     const { bookId, ...reviewDto } = body;
-    return this.reviewsService.create(id!, ReviewableType.Book, bookId, reviewDto);
+    return this.reviewsService.create(
+      id!,
+      ReviewableType.Book,
+      bookId,
+      reviewDto,
+    );
   }
 
   @ApiOperation({
-    summary: 'Get all reviews of a book'
+    summary: 'Get all reviews of a book',
+  })
+  @ApiOkResponse({
+    type: GetBookReviewsResponseDto,
   })
   @ApiQueryPagination()
   @ApiBearerAuth()
@@ -56,9 +85,42 @@ export class ReviewsController {
     @Param('id') id: string,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
-    @Req() req: Request
-  ) {    
+    @Req() req: Request,
+  ) {
     const { id: userId } = req.user ?? {};
-    return this.reviewsService.getAllReviews(userId, ReviewableType.Book, id, page, limit);
+    return this.reviewsService.getAllReviews(
+      userId,
+      ReviewableType.Book,
+      id,
+      page,
+      limit,
+    );
+  }
+
+  @ApiOperation({
+    summary: 'Get all reviews of a blog',
+  })
+  @ApiOkResponse({
+    type: GetBlogReviewsResponseDto,
+  })
+  @ApiQueryPagination()
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
+  @Serialize(GetBlogReviewsResponseDto)
+  @Get('blogs/:id')
+  async getBlogReviews(
+    @Param('id') id: string,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 10,
+    @Req() req: Request,
+  ) {
+    const { id: userId } = req.user ?? {};
+    return this.reviewsService.getAllReviews(
+      userId,
+      ReviewableType.Blog,
+      id,
+      page,
+      limit,
+    );
   }
 }
