b5b4bb412a6b81a94f7d09a2ba3541c4c969fe7d
Mohammad Hosseini <mimhe1381@gmail.com>
2025-08-06 18:02:27 +0330

    Add bookmark entity

---
+++ src/modules/books/entities/bookmark.entity.ts
@@ -0,0 +1,31 @@
+import { BaseEntity } from 'src/common/entities/base.entity';
+import { User } from 'src/modules/users/entities/user.entity';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Book } from './book.entity';
+
+export enum BookmarkTypes {
+  Read = 'read',
+  Loved = 'loved',
+  Library = 'library',
+}
+
+@Entity('bookmarks')
+export class Bookmark extends BaseEntity {
+  @Column('uuid')
+  userId: string;
+  @ManyToOne(() => User, (user) => user.bookmarks)
+  @JoinColumn()
+  user: User;
+
+  @Column('uuid')
+  bookId: string;
+  @ManyToOne(() => Book, (book) => book.bookmarks, { onDelete: 'CASCADE' })
+  book: Book;
+
+  @Column({
+    type: 'enum',
+    enum: BookmarkTypes,
+    default: BookmarkTypes.Library
+  })
+  type: BookmarkTypes;
+}
