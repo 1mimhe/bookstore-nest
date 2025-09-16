b5d714b26f6d6786cb1fd71951c5b989c2d718da
Mohammad Hosseini <mimhe1381@gmail.com>
2025-09-16 16:38:43 +0330

    Improve views types

--- src/modules/views/views.types.ts
+++ src/modules/views/views.types.ts
@@ -1,6 +1,11 @@
 
 export enum ViewEntityTypes {
   Title = 'title',
+  Author = 'author',
+  Publisher = 'publisher',
+  Blog = 'blog',
+  Tag = 'tag',
+  Collection = 'collection',
 }
 
 export interface ViewResult {
