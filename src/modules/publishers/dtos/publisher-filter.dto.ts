8687f064f47782d91b6849e171f372309893e9fa
Mohammad Hosseini <mimhe1381@gmail.com>
2025-09-18 14:22:56 +0330

    Improve get all publishers

---
+++ src/modules/publishers/dtos/publisher-filter.dto.ts
@@ -0,0 +1,36 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsEnum, IsInt, IsOptional, IsPositive, IsString } from 'class-validator';
+
+export enum SortBy {
+  NameAsc = 'name_asc',
+  NameDesc = 'name_desc',
+  MostView = 'mostview',
+  MostBooks = 'mostbooks',
+  Newest = 'newest'
+}
+
+export class PublisherFilterDto {
+  @Transform(({ value }) => Number(value))
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  page = 1;
+
+  @Transform(({ value }) => Number(value))
+  @IsOptional()
+  @IsInt()
+  @IsPositive()
+  limit = 10;
+
+  @ApiPropertyOptional({
+    description: 'Just a simple search in names'
+  })
+  @IsOptional()
+  @IsString()
+  search?: string;
+
+  @IsOptional()
+  @IsEnum(SortBy)
+  sortBy?: SortBy;
+}
