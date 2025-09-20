661a90fe2907ac1584f19c59e1f5ddc9fada48bb
Mohammad Hosseini <mimhe1381@gmail.com>
2025-09-20 14:58:18 +0330

    Improve collection entity

--- src/modules/collections/entities/collection.entity.ts
+++ src/modules/collections/entities/collection.entity.ts
@@ -1,6 +1,7 @@
 import { BaseEntity } from 'src/common/base.entity';
-import { Column, Entity, OneToMany, Unique } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany, Unique, JoinColumn } from 'typeorm';
 import { CollectionBook } from './collection-book.entity';
+import { User } from '../../users/entities/user.entity';
 
 @Entity()
 @Unique(['slug'])
@@ -22,4 +23,13 @@ export class Collection extends BaseEntity {
 
   @Column({ default: 0 })
   views: number;
+
+  @Column({ default: true })
+  isPublic: boolean;
+
+  @Column({ nullable: true })
+  userId?: string;
+  @ManyToOne(() => User, { nullable: true })
+  @JoinColumn()
+  user?: User;
 }
